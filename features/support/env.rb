068eac5fc5cd334e38417b98d1b42f0820264ee8
Persa Zula <persa@persazula.com>
2015-04-26 21:40:24 -0400

    Remove additions made while experimenting with VCR and Arubua::InProcess

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,19 +1,5 @@
 require 'cucumber'
 require 'aruba/cucumber'
-require 'aruba/in_process'
-require 'vcr'
-require 'webmock'
-require 'typekitable/runner'
-
-VCR.cucumber_tags do |t|
-  t.tag  '@vcr', :use_scenario_name => true
-end
-
-VCR.configure do |c|
-  c.hook_into :webmock
-  c.cassette_library_dir     = 'features/cassettes'
-  c.default_cassette_options = { :record => :new_episodes }
-end
 
 Before do
   @dirs = [File.expand_path(File.join(File.dirname(__FILE__), '../..'))]
@@ -26,16 +12,4 @@ end
 
 After '@mocked_home_directory' do
   FileUtils.rm_f ENV['HOME']
-end
-
-Before '@vcr'  do
-  Aruba::InProcess.main_class = Typekitable::Runner
-  Aruba.process = Aruba::InProcess
-end
-
-After '@vcr'  do
-  Aruba.process = Aruba::SpawnProcess
-  VCR.eject_cassette
-  $stdin = STDIN
-  $stdout = STDOUT
 end
